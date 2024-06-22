e390e5d0e2a9ccd63d2af00b8de9e93b1bba23b1
srikeerthis <srikeerthisrinivasan@gmail.com>
2024-06-22 02:01:10 -0500

    chore: updated paths and links

---
+++ src/consts.ts
@@ -0,0 +1,75 @@
+import type { Site, Page, Links, Socials } from "@types"
+
+// Global
+export const SITE: Site = {
+  TITLE: "Srikeerthi",
+  DESCRIPTION: "Welcome to my website",
+  AUTHOR: "Srikeerthi",
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: "Work",
+  DESCRIPTION: "Places I have worked.",
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: "Blog",
+  DESCRIPTION: "Writing on topics I am passionate about.",
+}
+
+// Projects Page 
+export const PROJECTS: Page = {
+  TITLE: "Projects",
+  DESCRIPTION: "Recent projects I have worked on.",
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: "Search",
+  DESCRIPTION: "Search all posts and projects by keyword.",
+}
+
+// Links
+export const LINKS: Links = [
+  { 
+    TEXT: "Home", 
+    HREF: "/", 
+  },
+  { 
+    TEXT: "Work", 
+    HREF: "/work", 
+  },
+  { 
+    TEXT: "Blog", 
+    HREF: "/blog", 
+  },
+  { 
+    TEXT: "Projects", 
+    HREF: "/projects", 
+  },
+]
+
+// Socials
+export const SOCIALS: Socials = [
+  { 
+    NAME: "Email",
+    ICON: "email", 
+    TEXT: "srikeerthi.vs@gmail.com",
+    HREF: "mailto:srikeerthi.vsv@gmail.com",
+  },
+  { 
+    NAME: "Github",
+    ICON: "github",
+    TEXT: "srikeerthis",
+    HREF: "https://github.com/srikeerthis"
+  },
+  { 
+    NAME: "LinkedIn",
+    ICON: "linkedin",
+    TEXT: "srikeerthis",
+    HREF: "https://www.linkedin.com/in/srikeerthis/",
+  },
+]
+
